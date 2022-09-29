f909965ec71b9b4e47e75e2a1e86367549b0c43a
taiyo <tsuzuki7660@conestogac.on.ca>
2022-09-29 13:27:13 -0400

    tweaks

--- Item 2/Source.c
+++ Item 2/Source.c
@@ -6,7 +6,7 @@
 //Item 2: Convert a user-input age in years to days and display both values
 
 //definitions
-#define DAYS_IN_YEAR 365												//define how many days are in one year
+#define DAYS_IN_YEAR 365														//define how many days are in one year
 
 int main(void) 
 {
@@ -14,10 +14,14 @@ int main(void)
 
 	printf("Enter your age in years: ");								
 
-	scanf_s("%d", &ageYears);											//scan for and save age input in years
+	if (scanf_s("%d", &ageYears) == 0)											//scan for user input and check if it is an integer, and if it is, save it
+	{
+		printf("\nInput not recognised as an integer, please try again.\n");	//if it is not an integer, exit the program
+		exit(0);
+	}																			
 
-	int ageDays = ageYears * DAYS_IN_YEAR;								//calculate age in days from age in years
+	int ageDays = ageYears * DAYS_IN_YEAR;										//calculate age in days from age in years
 
-	printf("\nYou are %d years or %d days old.\n", ageYears, ageDays);	//print age to screen in years and days
+	printf("\nYou are %d years or %d days old.\n", ageYears, ageDays);			//print age to screen in years and days
 	return 0;
 }
--- Item 3/Source.c
+++ Item 3/Source.c
@@ -7,23 +7,31 @@
 
 int main(void) 
 {
-	float numInput1, numInput2;																//create float variables to hold numbers
+	float numInput1 = 0, numInput2 = 0;													//create and initialize float variables to compare to input
 
-	printf("Please type first number and press enter:");									//prompt user to enter first number
+	printf("Please enter first number: ");												//prompt user to enter first number
 
-	scanf_s("%f", &numInput1);																//scan for and save first number
+	if (scanf_s("%f", &numInput1) == 0)													//scan for user input and check if it is a float, and if it is, save it
+	{
+		printf("\nInput not recognised as a valid float, please try again.\n");			//if it is not a float, exit the program
+		exit(0);
+	}															
 
-	printf("First number: %f\n\nPlease type second number and press enter:", numInput1);	//display first number and prompt user for second number
+	printf("First number: %f\n\nPlease enter second number: ", numInput1);				//display first number and prompt user for second number
 
-	scanf_s("%f", &numInput2);																//scan for and save second number
+	if (scanf_s("%f", &numInput2) == 0)													//scan for user input and check if it is a float, and if it is, save it
+	{
+		printf("\nInput not recognised as a valid float, please try again.\n");			//if it is not a float, exit the program
+		exit(0);
+	}																
 
-	printf("Second number: %f", numInput2);													//display second number
+	printf("Second number: %f", numInput2);												//display second number
 
-	float sum = numInput1 + numInput2;														//calculate sum, product, and quotient from user-input numbers
+	float sum = numInput1 + numInput2;													//calculate sum, product, and quotient from user-input numbers
 	float product = numInput1 * numInput2;
 	float quotient = numInput1 / numInput2;
 
-	printf("\n\nSum = %f\nProduct = %f\nQuotient = %f\n", sum, product, quotient);			//display sum, product, and quotient to screen
+	printf("\n\nSum = %f\nProduct = %f\nQuotient = %f\n", sum, product, quotient);		//display sum, product, and quotient to screen
 
 	return 0;
 }
